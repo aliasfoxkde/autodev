9777f347d9111893db4ff16818510b8a5ac0f5dc
Michael Kinney <Michael.Kinney@plains.com>
2024-11-30 00:16:15 -0600

    Fixing deployment

--- build/client/assets/_index-EveR0zF8.js
+++
@@ -1,1 +0,0 @@
-import{M as r,O as e}from"./_index-Bp79dlwj.js";import"./components-VKHSbR2h.js";import"./theme-9upYr29Y.js";export{r as default,e as meta};
--- build/client/assets/chat._id-BC_WozEE.js
+++
@@ -1,1 +0,0 @@
-import{M as p}from"./_index-Bp79dlwj.js";import"./components-VKHSbR2h.js";import"./theme-9upYr29Y.js";export{p as default};
